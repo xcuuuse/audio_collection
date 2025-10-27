40e35ad9862aa80a64fb4ec6dddc7eb57ad6c2bc
Alex Yevik <aleksejevik92@gmail.com>
2025-10-27 22:17:48 +0300

    lab2 (audio collection) first version

--- audio_collection/main.py
+++ audio_collection/main.py
@@ -1,30 +1,15 @@
-from audio.base import AudioFormat, AudioFile, Converter
-from artists.artist import Artist
-from artists.album import Album
-from audio.metadata import Genre, Tag
-from tracks.track import Track, LiveRecording
-from player.player import Player, AudioOutput
-form = AudioFormat(".wav")
-form2 = AudioFormat(".flac")
-file = AudioFile("e:/numb.wav", 183, form, 1024)
-print(file.get_info())
+"""
+playlist, collection, genre_collection, artist collection, liked(playlist), filemanager, user, userprofile?,
+username, password_manager, authentication_system, queue
+12 exceptions. 8"""
+from base.track import Genre, Track
+from base.albums.live_album import Album, LiveAlbum
+from base.artists.band import Artist, Band, Musician
+from playlists.playlist import Playlist
+from playlists.collections.genre_collection import Collection, GenreCollection
 
-converter = Converter([form, form2])
-converter.convert_file(file, form2)
-print(file.get_info())
-genres = [Genre("nu metal"), Genre("alt rock")]
-artist = Artist("Linkin Park", "linkin park", genres)
-album = Album("Meteora", 2003, artist, genres[0])
-# maybe do the logic of file size changing
-track = Track("e:/numb.wav", 183, form, 1024,
-              "Numb", artist, album)
-print(track.get_info())
-track.add_tag(Tag("metal"))
-print(track.get_info())
 
-device = AudioOutput("headphones")
 
-player = Player(track, device)
 
-player.play(track)
-player.play(track)
+
+
